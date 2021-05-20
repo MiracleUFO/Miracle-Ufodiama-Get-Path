e859289eb1c7827adfb7e7fd3e8fa7d9159f367b
MircleUFO <miracleufodiama@gmail.com>
2021-05-20 15:41:32 +0100

    Project structure

---
+++ index.js
@@ -0,0 +1,18 @@
+const a = {
+  user: {
+    id: 1,
+    name: {
+    firstName: "James",
+    lastName: "Ibori"
+    },
+    location: {
+      city: "Ikoyi",
+      state: "Lagos",
+      address: "One expensive house like that"
+    }
+  }
+}
+
+let handleSubmit = () => {
+  
+}
